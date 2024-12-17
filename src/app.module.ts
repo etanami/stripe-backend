3a31a7b14407ecc3aa009ceb4c38cdea61f98ef7
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-12-17 13:32:24 +0100

    refactor: payment controller and service call into the payment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,13 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PaymentsController } from './payments/payments.controller';
-import { PaymentsService } from './payments/payments.service';
 import { PaymentsModule } from './payments/payments.module';
 import * as bodyParser from 'body-parser';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PaymentsModule],
-  controllers: [AppController, PaymentsController],
-  providers: [AppService, PaymentsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
