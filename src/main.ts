f3c64c99672a632353dc9a51db5097cfa86df700
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-12-18 22:24:01 +0100

    update: cors frontend url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://magnificent-jelly-b397b9.netlify.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
