f5d9807aab72870834f8a776f83f487e2dd16cad
Etanami Olatunji <etanamiolatunji@gmail.com>
2025-03-26 18:57:32 +0100

    fix: use environment variable for prod instances

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  const configService = app.get(ConfigService);
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: configService.get('CORS_ORIGIN'),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
